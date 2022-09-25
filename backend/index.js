ddf2d6083150c0d6b73c5fc0816733c065b179f8
HugoHern <hugo8954@gmail.com>
2022-09-24 21:21:03 -0400

    connected backend to mongodb

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const bodyparser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const dotenv = require("dotenv");
 
 /*                   INITIALZING                */
 const app = express()
@@ -11,6 +12,26 @@ const app = express()
 app.use(bodyparser.json({limit: '30mb', extended: true}))
 app.use(bodyparser.urlencoded({limit: '30mb', extended: true}))
 app.use(cors())
+dotenv.config()
+
+/*                PORT          */
+const PORT = process.env.port || 5000
 
 /*                 MONGODB              */
-const CONNECTION_URL = process.env.MONGO_URI
+const connectDB =  async ()=> {
+
+    try{
+        await mongoose.connect("mongodb+srv://admin1:C87g0sfC2bDqIjYn@cluster0.f5j2chl.mongodb.net/?retryWrites=true&w=majority")
+        console.log(`mongo database is connected!!! `)
+    }catch(error){
+        console.error(`Error: ${error} `)
+    
+    }
+
+}
+
+connectDB()
+
+
+
+app.listen(PORT, () => console.log('server is running!'))
