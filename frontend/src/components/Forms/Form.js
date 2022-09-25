035640bee13caec64029def2bdd52dc3c0dc78cb
HugoHern <hugo8954@gmail.com>
2022-09-25 16:23:34 -0400

    fixed folder problem

---
+++ frontend/src/components/Forms/Form.js
@@ -0,0 +1,43 @@
+import React, {useState} from 'react'
+import useStyles from './Styles'
+import { TextField, Button, Typography, Paper } from '@material-ui/core'
+import FileBase from 'react-file-base64'
+
+function Form() {
+
+  const [postData, setPostData] = useState({
+    creator: '',
+    title: '',
+    message: '',
+    tags: '',
+    selectedfile: ''
+  })
+
+  const classes = useStyles()
+
+  const handleSubmit = () => {
+    console.log('form submitted')
+  }
+
+  const clear = () => {
+    console.log('clearing form')
+  }
+
+
+  return (
+    <Paper className={classes.paper}>
+      <form autoComplete='off' noValidate className={classes.form} onSubmit={handleSubmit}>
+        <Typography variant='h6'>Creating a Memory</Typography>
+        <TextField name='creator' variant='outlined' label='Creator' fullWidth value={postData.creator} onChange={(e)=> setPostData({...postData, creator: e.target.value})}/>
+        <TextField name='title' variant='outlined' label='Title' fullWidth value={postData.title} onChange={(e)=> setPostData({...postData, creator: e.target.value})}/>
+        <TextField name='message' variant='outlined' label='Message' fullWidth value={postData.message} onChange={(e)=> setPostData({...postData, creator: e.target.value})}/>
+        <TextField name='tags' variant='outlined' label='Tags' fullWidth value={postData.tags} onChange={(e)=> setPostData({...postData, creator: e.target.value})}/>
+        <div className={classes.fileInput}><FileBase type='file' multiple={false} onDone={(base64) => setPostData({...postData, selectedfile: base64})}/></div>
+        <Button className={classes.buttonSubmit} variant='contained' color='primary' size='large' type='submit' fullWidth>Submit</Button>
+        <Button className={classes.buttonSubmit} variant='contained' color='secondary' size='small' onClick={clear} fullWidth>Clear</Button>
+      </form>
+    </Paper>
+  )
+}
+
+export default Form
