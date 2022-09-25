95bd0d71d427184a8026ade7bfb441639483fa4e
HugoHern <hugo8954@gmail.com>
2022-09-25 19:32:19 -0400

    added icons from material ui

--- frontend/src/components/Posts/Post/Post.js
+++ frontend/src/components/Posts/Post/Post.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import useStyles from './Styles'
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core'
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import moment from 'moment'
 
-function Post() {
+const Post = ({post}) => {
   const classes = useStyles()
   return (
-    <div>Post</div>
+    <Card className={classes.card}>
+      <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
+      <div className={classes.overlay}>
+        <Typography variant='h6'>{post.creator}</Typography>
+        <Typography variant='body2'>{moment(post.createdAt).fromNow()}</Typography>
+      </div>
+
+    </Card>
   )
 }
+
 
 export default Post
--- frontend/src/components/Posts/Posts.js
+++ frontend/src/components/Posts/Posts.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Post from './Post/Post'
 import useStyles from './Styles'
 import { useSelector } from 'react-redux'
+import { Grid, CircularProgress } from '@material-ui/core'
 
 
 function Posts() {
@@ -9,12 +10,18 @@ function Posts() {
   const classes = useStyles()
   console.log(posts)
   return (
-    <>
-    <h1>POSTS</h1>
-    <Post/>
-    <Post/>
-    </>
+    !posts.length ? <CircularProgress /> : (
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post={post}/>
+          </Grid>
+        ))}
+
+      </Grid>
+    )  
   )
 }
+
 
 export default Posts
